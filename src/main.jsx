69ce1c9f0b146cfadcca67cd03f13196bc8a94c1
vikasreddy148 <kathulavikasr@gmail.com>
2025-03-29 19:23:32 +0530

    my second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path='/' element={<App/>}/>
-        
+        <Route path="/" element={<App />} />
       </Routes>
-    </BrowserRouter>
-
-  </React.StrictMode>,
-)
+    </HashRouter>
+  </React.StrictMode>
+);
